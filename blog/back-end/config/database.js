132921365196295b2df7aa689f14093387937f7b
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2023-01-16 19:14:41 +0100

    commit init-front

--- blog/back-end/config/database.js
+++ blog/back-end/config/database.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 //connexion à la base de données
-mongoose.connect("mongodb://0.0.0.0:27017/db");
+mongoose.connect(
+  "mongodb+srv://Thomas:XhKnNiUrOCJWmocJ@cluster0.n7udm.mongodb.net/test"
+);
 
 mongoose.connection.on("error", () => {
   console.log("Erreur lors de la connexion à la base de données");
@@ -11,6 +13,10 @@ mongoose.connection.on("error", () => {
 mongoose.connection.on("open", () => {
   console.log("Connexion à la base de données établie");
 });
+
+// // Replace the uri string with your connection string.
+// const uri = "mongodb+srv://cluster0.n7udm.mongodb.net/myFirstDatabase";
+// const client = new mongoose(uri);
 
 let ArticleSchema = mongoose.Schema({
   title: String,
