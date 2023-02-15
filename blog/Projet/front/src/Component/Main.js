7fd2f850d1c1099fc400fd93793647d2bd777cbc
Thomas <thomas.miramont@livecampus.tech>
2023-02-15 15:05:47 +0100

    commit frontUpdate

--- blog/Projet/front/src/Component/Main.js
+++ blog/Projet/front/src/Component/Main.js
@@ -1,7 +1,16 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Main = () => {
-  // const [liste, setListe] = useState([]);
+  const [liste, setListe] = useState([]);
+
+  useEffect(() => {
+    fetch("/getPost")
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        setListe(res);
+      });
+  }, []);
 
   return (
     <main>
@@ -10,6 +19,15 @@ const Main = () => {
         Bienvenue sur mon portfolio ! Vous trouverez ici tous mes projets
         personnel et professionnel
       </p>
+      <ul className="home-list">
+        {liste.map((item) => (
+          <li key={item._id}>
+            <a href="/">{item.title}</a>
+            <img src={item.images} alt={item.title} width="20%" />
+            <p>{item.description.substring(0, 30)}...</p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
