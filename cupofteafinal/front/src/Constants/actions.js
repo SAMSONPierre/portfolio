ab4d6ff65ad5d39e1d6d11d7e56ae7430e707456
Thomas <thomas.miramont@livecampus.tech>
2023-02-22 12:39:14 +0100

    commit Update

--- cupofteafinal/front/src/Constants/actions.js
+++
@@ -1,6 +0,0 @@
-//liste des actions possibles du reducer
-export const ADD_TEA = 'ADD_TEA';
-export const DELETE_TEA = 'DELETE_TEA';
-export const DELETE_ALL = 'DELETE_ALL';
-export const CONNECT_USER = 'CONNECT_USER';
-export const DECONNECT_USER = 'DECONNECT_USER';
